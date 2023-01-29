5b6f7e8fdd611f05a36fc29203e6099e13fcee44
thakurmandar86 <78611522+thakurmandar86@users.noreply.github.com>
2023-01-29 23:08:41 +0530

    update

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
 const fultooBtn = document.getElementById('FULTOOO');
 const btn1 = document.getElementById('clickHereBtn');
+const btn2 = document.getElementById('clickHereAgainBtn');
 const cardsContainer = document.getElementById('cards');
 const aboutContainer = document.getElementById('about');
 const letterVideo = document.getElementById('letterVideo');
+
+const vidss = document.getElementById('vidssss');
+const video2Container = document.getElementById('video2');
+
 
 let wrapperRect;
 let noBtnRect;
@@ -16,25 +21,41 @@ console.log(wrapperRect, noBtnRect);
 btn1.addEventListener('click', function() {
     aboutContainer.style.display = 'block';
 
-    setTimeout(myAutoScroll, 1000)
+    setTimeout(myAutoScroll(), 1000)
 
     wrapperRect = wrapper.getBoundingClientRect();
     noBtnRect = noBtn.getBoundingClientRect();
 
 })
 
-fultooBtn.addEventListener('click', function() {
+btn2.addEventListener('click', function() {
     cardsContainer.style.display = 'flex';
 
-      setTimeout(myAutoScroll, 1000)
+    setTimeout(myAutoScroll(), 1000)
 
-    console.log(cardsContainer.scroll);
+    wrapperRect = wrapper.getBoundingClientRect();
+    noBtnRect = noBtn.getBoundingClientRect();
 })
 
-const myAutoScroll = () => {
-    cardsContainer.scrollIntoView({
-        behavior: 'smooth'
-    });
+fultooBtn.addEventListener('click', function() {
+    video2Container.style.display = 'flex';
+
+      setTimeout(myAutoScroll('video'), 1000)
+
+    console.log(video2Container.scroll);
+})
+
+const myAutoScroll = (element) => {
+
+    if(element == 'video') {
+        video2Container.scrollIntoView({
+            behavior: 'smooth'
+        });
+    } else {
+        cardsContainer.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
 }
 
 yesBtn.addEventListener('click', () => {
@@ -53,5 +74,9 @@ noBtn.addEventListener('mouseover', () => {
 
 letterVideo.addEventListener('ended', function() {
     var content = document.getElementsByClassName('content');
+    content[0].style.display = 'block'
+})
+vidss.addEventListener('ended', function() {
+    var content = document.getElementsByClassName('fultoo');
     content[0].style.display = 'block'
 })
